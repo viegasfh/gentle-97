f0e1a9ec595e8fb5de752d29e1f9d657f3db6f90
Fidel H Viegas <me@fidelhviegas.com>
2021-11-29 18:58:17 +0100

    Modified the K&R syntax to C99

--- lib/main.c
+++ lib/main.c
@@ -2,35 +2,28 @@
 
 extern FILE *yyin;
 
-main (argc, argv)
-   int argc;
-   char **argv;
+int main(int argc, char **argv) {
 
-{
+  /* INITIALIZE */
 
-   /* INITIALIZE */
+  if (argc > 2) {
+    printf("too many arguments\n");
+    exit(1);
+  }
 
-   if (argc > 2) {
-      printf("too many arguments\n");
+  if (argc == 2) {
+    yyin = fopen(argv[1], "r");
+    if (yyin == NULL) {
+      printf("cannot open '%s'\n", argv[1]);
       exit(1);
-   }
+    }
+  }
 
-   if (argc == 2) {
-      yyin = fopen (argv[1], "r");
-      if (yyin == NULL) {
-	 printf("cannot open '%s'\n", argv[1]);
-	 exit(1);
-      }
-   }
+  /* INVOKE GENERATED PROGRAM */
 
+  ROOT();
 
-   /* INVOKE GENERATED PROGRAM */
+  /* FINALIZE */
 
-   ROOT();
-
-   
-
-   /* FINALIZE */
-
-   exit(0);
+  return 0;
 }
